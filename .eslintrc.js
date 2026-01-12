eed0237e5c6e59514a06a3d3ff9c59ea47c4ef7a
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2026-01-11 21:56:09 -0500

    first set up and dependency update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,7 @@ module.exports = {
     'es2021': true,
     'node': true,
   },
-  'extends': [
-    'google',
-  ],
+  'extends': ['google', 'plugin:storybook/recommended'],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaVersion': 12,
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,34 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+
 const config: StorybookConfig = {
+  framework: "@storybook/react-webpack5",
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  docs: {
-    autodocs: true,
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+  webpackFinal: async (config) => {
+    config.module = config.module || { rules: [] };
+    config.module.rules = config.module.rules || [];
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [
+            require.resolve("@babel/preset-env"),
+            require.resolve("@babel/preset-react"),
+            require.resolve("@babel/preset-typescript"),
+          ],
+        },
+      },
+    });
+
+    config.resolve = config.resolve || {};
+    config.resolve.extensions = config.resolve.extensions || [".js"];
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    return config;
   },
 };
+
 export default config;
